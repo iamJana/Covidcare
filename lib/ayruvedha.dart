40197a99df00eadc6dd6598a8757e0279c8db0ed
jana <janamurugesan01@gmail.com>
2021-05-05 19:46:16 +0530

    Done with basic ui

--- lib/ayruvedha.dart
+++ lib/ayruvedha.dart
@@ -6,16 +6,14 @@ class Ayurvedha extends StatefulWidget {
   _AyurvedhaState createState() => _AyurvedhaState();
 }
 
-const tc = Color(0xFFFF4848);
+const tc = Color(0xffffb01a);
 
 class _AyurvedhaState extends State<Ayurvedha> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ayurvedha'),
-        backgroundColor: Color(0xff6c63ff),
-      ),
+      appBar:
+          AppBar(title: Text('Ayurvedha'), backgroundColor: Color(0xff2C5F2D)),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -26,7 +24,14 @@ class _AyurvedhaState extends State<Ayurvedha> {
             buildColoredCard('Nasalcongestion(Pratishyaya)', 'Haridrakhand-',
                 '3 - 5 gm BD with lukewarm water/ milk', tc),
             buildColoredCard('Myalgia(Parshwa - shoola,Shirashoola,Angamarda)',
-                'Devadaryadikwatha', '30 - 40 ml BD before food', tc)
+                'Devadaryadikwatha', '30 - 40 ml BD before food', tc),
+            buildColoredCard(
+                'Cough(Vataja Kasa)',
+                'Talisadi Churna ',
+                '(4 g)+ Madhuyashtichurna (2 g) BDwith honey/ lukewarm water',
+                tc),
+            buildColoredCard('Dehydrationfeatures (Trishanadue to Jwara)',
+                'Shadangapaneeya', '40 ml tid/as per requirement', tc),
           ],
         ),
       ),
