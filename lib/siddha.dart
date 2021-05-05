42ab32d3833b74fe15a968561d6237bdaeefcffb
Ishwarya <ishudevi2000@gmail.com>
2021-05-05 17:57:09 +0530

    adobe

--- lib/siddha.dart
+++ lib/siddha.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 
 class Siddha extends StatefulWidget {
   @override
@@ -11,7 +11,54 @@ class _SiddhaState extends State<Siddha> {
     return Scaffold(
       appBar: AppBar(title: Text('Siddha'),
       backgroundColor: Color(0xff6c63ff),
+      
+      ),
+      body: Column(
+        children: [
+          buildColoredCard()
+        ],
       ),
     );
   }
 }
+Widget buildColoredCard() => Card(
+        shadowColor: Colors.red,
+        elevation: 8,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.redAccent, Colors.red],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Colored card',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                'This card is rounded and has a gradient',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+
+  
